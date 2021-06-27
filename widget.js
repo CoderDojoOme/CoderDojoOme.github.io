9ec9b8c3a95c9475e9d4dd480a7f6db7e59cfd1c
IchiroKano <ichiro.kano2@asahi.email.ne.jp>
2021-06-27 12:48:49 +0900

    member iframeやめて、javascript

---
+++ widget.js
@@ -0,0 +1,14 @@
+
+function widgetstart() {
+    let strHTML = ''
+        + '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>'
+        + '<div class="card" style="width: 200px;">'
+        + '<div class="card bg-primary text-white">'
+        + '<h6 class="card-header">APPタイトル</h6>'
+        + '</div>'
+        + '<div class="card-body">'
+        + '<p>コンテンツをここに作る</p>'
+        + '</div>'
+        + '</div>';
+    document.write(strHTML);
+}
